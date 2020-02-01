f2fe1cd3a93f8bee5dee5424c6700a88c5442950
MikeJConner <mikeJconner6@gmail.com>
2020-02-01 13:26:38 -0800

    set the framework for the project

---
+++ snake.py
@@ -0,0 +1,46 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class Cube(object):
+    def __init__(self, start, direcX=1, direcY=0, color(200,200,200)):
+        pass
+
+    def move(self, direcX, direcY):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class Snake(object):
+    def __init__(self, color, position):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, position):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+def drawGrid(screenWidth, rows, surface):
+    pass
+
+def redrawWindow(surface):
+    pass
+
+def createSnack(rows, item):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main()__":
+    main()
